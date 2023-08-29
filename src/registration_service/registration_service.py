045a8831e90058118e40cc8c395a0558f7d90cb2
Ricky512227 <kamalsaidevarapalli@gmail.com>
2023-08-29 02:38:33 -0700

    images fetching technique is added

--- src/registration_service/registration_service.py
+++ src/registration_service/registration_service.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         registration_app_logger.info("Started the REGISTRATION server ...")
         registration_app_logger.info("Binded REGISTRATION-SERVICE at IP-ADDRESS:PORT :: {0}:{1}".format(registration_app.config['REGISTRATION_SERVER_IPADDRESS'],registration_app.config['REGISTRATION_SERVER_PORT']))
         registration_app_logger.info("Application is ready to server traffic.")
-        # registration_app.run(debug=registration_app.config["DEBUG"], port=registration_app.config["REGISTRATION_SERVER_PORT"], host=registration_app.config['REGISTRATION_SERVER_IPADDRESS'])
         registration_app.run()
     except Exception as ex:
         print("Error occurred :: {0}\tLine No:: {1}".format(ex, sys.exc_info()[2].tb_lineno))
